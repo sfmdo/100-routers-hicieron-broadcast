e094e31dd7e1f65dba9a98bc1d2cf39d4d7e9a3a
sfmdo <santiagomontesdeo@gmail.com>
2025-12-03 13:22:00 -0600

    Se removio codigo no usado de las rutas
    Removed commented-out HttpResponse code from views.

--- paginaredes/paginaredes/views.py
+++ paginaredes/paginaredes/views.py
@@ -1,17 +1,5 @@
 #from django.http import HttpResponse
 from django.shortcuts import render,redirect
 
-def dashboard(request):
-    #return HttpResponse("Hello World")
-    return render(request, 'dashboard.html')
-
-def calendar(request):
-    #return HttpResponse("El pepe ete sech")
-    return render(request, 'calendar.html')
-
-def tasks(request):
-    #return HttpResponse("Ete sech")
-    return render(request, 'tasks.html')
-
 def redirectToDashboard(request):
     return redirect('dashboard/')
